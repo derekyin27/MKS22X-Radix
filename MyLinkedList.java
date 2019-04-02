911e8a8f57f909e0b82e29797e2334921852a07f
Derek <dyin00@stuy.edu>
2019-04-02 11:47:49 -0400

    changed methods

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -1,45 +1,7 @@
 public class MyLinkedList<E>{
-  private class Node<E>{
-    private Node<E> next, prev;
-    private Integer data;
-    public Node(Integer dat, Node<E> nextup, Node<E> previous){
-      data = dat;
-      next = nextup;
-      previous = prev;
-    }
-    Node(Integer dat){
-      data = dat;
-      next = null;
-      prev = null;
-    }
-    public Node<E> next(){
-      return next;
-    }
-    public Node<E> prev(){
-      return prev;
-    }
-    public void setNext(Node<E> other){
-      next = other;
-    }
-    public void setPrev(Node<E> other){
-      prev = other;
-    }
-    public Integer getData(){
-      return data;
-    }
-    public Integer setData(Integer i){
-      Integer x = data;
-      data = i;
-      return x;
-    }
-    public String toString(){
-      return "" + data;
-    }
-  }
 
-private Node start, end;
-private int size;
-//private
+  private int size;
+  private Node<E> start,end;
 
   public MyLinkedList(){
     size = 0;
@@ -48,85 +10,64 @@ private int size;
   public int size(){
     return size;
   }
+  public void clear(){
+    size = 0;
+  }
+    public String toString(){
+      if (size == 0){
+        return "[]";
+      }
+      String ans = "[";
+      //transversing list
+      Node<E> current = start;
+      while(current != null){
+        ans += (current.getData()+", ");
+        current = current.next();
+      }
+      return ans.substring(0,ans.length()-2)+"]";
+    }
 
-  public boolean add(Integer value){
-    Node<E> yeet = new Node(value);
+  private Node<E> get(int n){
+    Node<E> current = start;
+    for (int i = 0; i < n; i++){
+      current = current.next();
+    }
+    return current;
+  }
+  
+  public boolean add(E value){
+    Node<E> n = new Node<E>(value);
+    //special case if list is empty - node would be both start and end node
+    if (size == 0){
+      start = n;
+      end = n;
+    }
+    else{
+      end.setNext(n);
+      n.setPrev(end);
+      end = n;
+    }
     size++;
     return true;
   }
 
-  public String toString(){
-    String str = "";
-    Node<E> current = start;
-    for (int i = 0; i < size; i++){
-      str += current.getData();
-      current = current.next();
+  public E removeFront(){
+    Node<E> n = start;
+    if(size>1){
+      start = start.next();
+      start.setPrev(null);
     }
-    return str;
+    size--;
+    return n.getData();
   }
 
-  public Integer get(int index){
-    if (size <= 0)
-    throw new IndexOutOfBoundsException();
-    Node<E> current = start;
-    for (int i = 0; i < index; i++){
-    current = current.next();
+  public void extend(MyLinkedList<E> other){
+    end.setNext(other.start);
+    end = other.end;
+    size+= other.size();
+    other.clear();
   }
-  return current.getData();
-}
 
-public Integer set(int index, Integer value){
-  if (index > size)
-  throw new IndexOutOfBoundsException();
 
-  Node<E> finals = start;
-  for (int i = 0; i < index; i++){
-    finals = finals.next();
-  }
-  Integer toReturn = finals.getData();
-  finals.setData(value);
 
-  Node<E> previously = start;
-  for (int i = 0; i < index - 1; i++){
-    previously = previously.next();
-  }
-  previously.setNext(finals);
-
-  Node<E> nextly = start;
-  for (int i = 0; i < index + 1; i++){
-    nextly = nextly.next();
-  }
-  nextly.setPrev(finals);
-
-  return toReturn;
-}
-
-public boolean contains(Integer value){
-  Node<E> current = start;
-  for (int i = 0; i < size; i++){
-    if (current.getData() == value)
-    return true;
-    else{
-      current = current.next();
-    }
-  }
-  return false;
-}
-public void clear(){
-  start=null;
-  end = null;
-size =0;
-}
-public void extend(MyLinkedList<E> other){
-  this.end = other.start;
-  this.size+=other.size;
-  other.size=0;
-}
-public Integer removeFront(){
-  Node<E> toRemove = start;
-  start = start.next();
-  size--;
-  if (size==0) end = null;
-  return toRemove.getData();
-}
 }
