e8a3b9e39d9bde21ba65f655148a4a153043c00d
Derek <dyin00@stuy.edu>
2019-04-02 23:26:21 -0400

    creating buckets

--- Radix.java
+++ Radix.java
@@ -1,3 +1,11 @@
+import java.util.*;
 public class Radix{
-  
+  @SuppressWarnings("unchecked")
+  public static void radixsort(int[] data){
+    MyLinkedList<Integer>[] bucket = new MyLinkedList[20];//extend to include negatives
+    MyLinkedList<Integer>[] temp = new MyLinkedList<>();
+    for (int i =0; i < bucket.length; i++){
+      bucket[i] = new MyLinkedList<Integer>();
+    }
+  }
 }
