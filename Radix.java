1fddf4d2ba8113751b19b6689b413489593fcb81
Derek <dyin00@stuy.edu>
2019-04-02 23:34:25 -0400

    working on sorting

--- Radix.java
+++ Radix.java
@@ -7,5 +7,18 @@ public class Radix{
     for (int i =0; i < bucket.length; i++){
       bucket[i] = new MyLinkedList<Integer>();
     }
+    for (int i =0; i<data.length; i++){
+      temp.add(data[i]);
+    }
+    int max = Math.abs(data[0]);
+    for (int i =0; i <data.length; i++){
+      if (Math.abs(data[i])>max) max = Math.abs(data[i]);
+    }
+    for (int in = 1; max/in > 0; in*=10){
+      for (int i =0; i < data.length; i++){
+        int x = temp.removeFront();
+        bucket[9+n/place%10].add(x);
+      }
+    }
   }
 }
