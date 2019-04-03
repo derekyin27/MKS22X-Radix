fe9ded699ff966b288ea676b746c63db7ae2f79e
Derek <dyin00@stuy.edu>
2019-04-02 22:59:47 -0400

    node class and tostring

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -1,73 +1,54 @@
 public class MyLinkedList<E>{
+  private class Node<E>{
+  private E data;
+  private Node<E> next;
+  private Node<E> prev;
+  public Node(E data1, Node<E> next1, Node<E> prev1){
+    data = data1;
+    next = next1;
+    prev = prev1;
+  }
+  public Node (E data1){
+    data = data1;
+  }
+  public Node<E> next(){
+    return next;
+  }
+  public Node<E> prev(){
+    return prev;
+  }
+  public void setNext(Node<E> next1){
+    next = next1;
+  }
+  public void setPrev(Node<E> prev1){
+    prev = prev1;
+  }
+  public E getData(){
+    return data;
+  }
+  public void setData(E data1){
+    data = data1;
+  }
+  public String toString(){
+  	return data+"";
+  }
+
+}
 
   private int size;
   private Node<E> start,end;
 
-  public MyLinkedList(){
-    size = 0;
-  }
-
-  public int size(){
-    return size;
-  }
-  public void clear(){
-    size = 0;
-  }
-    public String toString(){
-      if (size == 0){
-        return "[]";
-      }
-      String ans = "[";
-      //transversing list
-      Node<E> current = start;
-      while(current != null){
-        ans += (current.getData()+", ");
-        current = current.next();
-      }
-      return ans.substring(0,ans.length()-2)+"]";
-    }
-
-  private Node<E> get(int n){
-    Node<E> current = start;
-    for (int i = 0; i < n; i++){
+  public String toString(){
+    String str = "[";
+    Node current = start;
+    if (end == null) return str + "]";
+    while (current.next()!= null){
+      str+=current.getData() + ",";
       current = current.next();
     }
-    return current;
+    str += current.getData() + "]";
+    return str;
   }
-  
-  public boolean add(E value){
-    Node<E> n = new Node<E>(value);
-    //special case if list is empty - node would be both start and end node
-    if (size == 0){
-      start = n;
-      end = n;
-    }
-    else{
-      end.setNext(n);
-      n.setPrev(end);
-      end = n;
-    }
-    size++;
-    return true;
-  }
-
-  public E removeFront(){
-    Node<E> n = start;
-    if(size>1){
-      start = start.next();
-      start.setPrev(null);
-    }
-    size--;
-    return n.getData();
-  }
-
-  public void extend(MyLinkedList<E> other){
-    end.setNext(other.start);
-    end = other.end;
-    size+= other.size();
-    other.clear();
-  }
-
 
 
 }
